5953ddfaed98a3a2444aa62d588ef3f4f3b09094
ChristopherMillones <ChristopherMillones@hotmail.com>
2020-03-09 01:10:10 -0700

    Assignment 2 Completed

--- Assignment2.sql
+++ Assignment2.sql
@@ -1,4 +1,31 @@
 select * from employee;
 insert into employee(Fname,Minit,Lname,Ssn,Bdate,Address,Sex,Salary,Super_ssn,Dno)
-values('Franklin','T','Wong','333445555','1955-12-08','638 Voss,Houston,TX','M',40000,'888665555',5);
+values('James','E','Borg','888665555','1937-11-10','450 Stone, Houston,TX', 'M', 55000, NULL,1);
+
+select * from department;
+insert into department(Dname,Dnumber,Mgr_ssn,Mgr_start_date)
+values('Headquesters',1,'888665555','1981-06-19');
+
+select * from dept_locations;
+insert into dept_locations(Dnumber,Dlocation)
+values(5,'Houston');
+
+
+select * from works_on;
+SET FOREIGN_KEY_CHECKS=0;
+insert into works_on(Essn,Pno,Hours)
+values('987987987',30,5.0);
+SET FOREIGN_KEY_CHECKS=1;
+
+
+select * from project;
+insert into project(Pname,Pnumber,Plocation,Dnum)
+values('Newbenefits',30,'Stafford',4);
+
+select * from dependent;
+insert into dependent(Essn,Dependent_name,Sex,Bdate,Relationship)
+values('123456789','Elizabeth','F','1967-05-05','Spouse');
+
+
+
 
--- company.sql
+++ company.sql
@@ -40,6 +40,7 @@ CREATE TABLE `department` (
 
 LOCK TABLES `department` WRITE;
 /*!40000 ALTER TABLE `department` DISABLE KEYS */;
+INSERT INTO `department` VALUES ('Headquesters',1,'888665555','1981-06-19'),('Administration',4,'987654321','1995-01-01'),('Research',5,'333445555','1988-05-22');
 /*!40000 ALTER TABLE `department` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -67,6 +68,7 @@ CREATE TABLE `dependent` (
 
 LOCK TABLES `dependent` WRITE;
 /*!40000 ALTER TABLE `dependent` DISABLE KEYS */;
+INSERT INTO `dependent` VALUES ('123456789','Alice','F','1988-12-30','Daughter'),('123456789','Elizabeth','F','1967-05-05','Spouse'),('123456789','Michael','M','1988-01-04','Son'),('333445555','Alice','F','1986-04-05','Daughter'),('333445555','Joy','F','1958-05-03','Spouse'),('333445555','Theodore','M','1983-10-25','Son'),('987654321','Abner','M','1942-02-28','Spouse');
 /*!40000 ALTER TABLE `dependent` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,6 +93,7 @@ CREATE TABLE `dept_locations` (
 
 LOCK TABLES `dept_locations` WRITE;
 /*!40000 ALTER TABLE `dept_locations` DISABLE KEYS */;
+INSERT INTO `dept_locations` VALUES (1,'Houston'),(4,'Stafford'),(5,'Bellaire'),(5,'Houston'),(5,'Sugarland');
 /*!40000 ALTER TABLE `dept_locations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +125,7 @@ CREATE TABLE `employee` (
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-INSERT INTO `employee` VALUES ('John','B','Smith','123456789','1965-01-09','731 Fondren, Houston,TX','M',30000.00,'333445555',5),('Franklin','T','Wong','333445555','1955-12-08','638 Voss,Houston,TX','M',40000.00,'888665555',5);
+INSERT INTO `employee` VALUES ('John','B','Smith','123456789','1965-01-09','731 Fondren, Houston,TX','M',30000.00,'333445555',5),('Franklin','T','Wong','333445555','1955-12-08','638 Voss,Houston,TX','M',40000.00,'888665555',5),('Joyce','A','English','453453453','1972-07-31','5631 Rice, Houseton,TX','F',25000.00,'333445555',5),('Ramesh','K','Narayan','666884444','1962-09-15','975 Fire Oak, Humble,TX','M',3800.00,'333445555',5),('James','E','Borg','888665555','1937-11-10','450 Stone, Houston,TX','M',55000.00,NULL,1),('Jennifer','S','Wallace','987654321','1941-06-20','291 Berry, Bellaire,TX','F',43000.00,'888665555',4),('Ahmad','V','Jabbar','987987987','1969-03-29','980 Dallas, Houston,TX','M',25000.00,'987654321',4),('Alicia','J','Zelaya','999887777','1968-01-19','3321 Castle, Spring, TX','F',25000.00,'987654321',4);
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -151,6 +154,7 @@ CREATE TABLE `project` (
 
 LOCK TABLES `project` WRITE;
 /*!40000 ALTER TABLE `project` DISABLE KEYS */;
+INSERT INTO `project` VALUES ('ProuctX',1,'Bellaire',5),('ProuctY',2,'Sugarland',5),('ProuctZ',3,'Houston',5),('Computerization',10,'Stafford',4),('Reorganization',20,'Houston',1),('Newbenefits',30,'Stafford',4);
 /*!40000 ALTER TABLE `project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,6 +182,7 @@ CREATE TABLE `works_on` (
 
 LOCK TABLES `works_on` WRITE;
 /*!40000 ALTER TABLE `works_on` DISABLE KEYS */;
+INSERT INTO `works_on` VALUES ('123456789',1,32.5),('123456789',2,7.5),('333445555',2,10.0),('333445555',3,10.0),('333445555',10,10.0),('333445555',20,10.0),('453453453',1,20.0),('453453453',2,20.0),('666884444',4,40.0),('987987987',10,35.0),('987987987',30,5.0),('999887777',10,10.0),('999887777',30,30.0);
 /*!40000 ALTER TABLE `works_on` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -190,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-02-28  0:37:21
+-- Dump completed on 2020-03-09  1:08:12
