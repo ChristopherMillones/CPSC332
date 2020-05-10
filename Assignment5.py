211a2844cb6464fbde8a3ede53d66e45bcbc1c19
ChristopherMillones <ChristopherMillones@hotmail.com>
2020-05-09 20:06:27 -0700

    Progress

--- Assignment5.py
+++ Assignment5.py
@@ -13,6 +13,8 @@ class Art_Gallery:
                 self.cursor = self.cxn.cursor(prepared=True)
                 self.cursor.execute("select database();")
                 record = self.cursor.fetchone()
+            self.cursor.execute("""SELECT TABLES""")
+            self.tables = self.cursor.fetchall()
         except Error as e:
             print("Error while connecting to MYSQL", e)
 
@@ -22,92 +24,41 @@ class Art_Gallery:
             self.cxn.close()
 
     def displayTable(self, table):
-        # SELECTING a TABLE and Displaying Information
-        self.cursor.execute("""SELECT * FROM {tab}""".format(tab=table))
-        record = self.cursor.fetchall()
-        output(table, record)
+        for i in range(len(self.tables)):
+            if table == self.tables[i]:
+                # SELECTING a TABLE and Displaying Information
+                self.cursor.execute("""SELECT * FROM {tab}""".format(tab=table))
+                record = self.cursor.fetchall()
+            else:
+                print("ERROR TABLE NOT FOUND")
 
-    def selectRecord(self, FLname):
-        self.cursor.execute("""SELECT * FROM artist WHERE FLname= '{name}'""".format(name=FLname))
+    def selectRecordFromArtist(self, Data):
+        self.cursor.execute("""SELECT * FROM artist WHERE FLname= '{name}' OR phone= '{data}' OR address= '{data}' 
+        OR birthPlace= '{data}' OR age= '{data'}' OR styleOfArt= '{data}'""".format(data=Data))
         record = self.cursor.fetchall()
-        output('artist', record)
+
+    def selectRecordFromArtShows(self, Data):
+        self.cursor.execute("""SELECT * FROM artshows WHERE contactNumber= '{name}' OR contact= '{data}' OR location= '{data}' 
+         OR dateShow= '{data}' OR timeShow= '{data'}'""".format(data=Data))
+        record = self.cursor.fetchall()
+
+    def selectRecordFromArtWork(self, Data):
+        self.cursor.execute("""SELECT * FROM artwork WHERE typeOfArt= '{name}' OR dateCreated= '{data}' OR dateAcquired= '{data}' 
+         OR price= '{data}' OR location= '{data'}' OR title='{data{'""".format(data=Data))
+        record = self.cursor.fetchall()
+
+    def selectRecordFromCustomer(self, Data):
+        self.cursor.execute("""SELECT * FROM Customer WHERE artPreference= '{name}' OR phone= '{data}' 
+        OR customerNumber= '{data}' """.format(data=Data))
+        record = self.cursor.fetchall()
+
 
     def sortASC(self):
         self.cursor.execute("""SELECT * FROM customer ORDER BY customerNumber ASC """)
         record = self.cursor.fetchall()
         print('\n')
-        output('customer', record)
 
     def sortDESC(self):
         self.cursor.execute("""SELECT * FROM customer ORDER BY customerNumber DESC """)
         record = self.cursor.fetchall()
         print('\n')
-        output('customer', record)
-
-
-
-def action(i):
-    db = Art_Gallery("root", "Python1!", "art_gallery")
-    if i == "1":
-        table_choice = input("\t---Enter Table---\n")
-        db.displayTable(table_choice.lower())
-    elif i == "2":
-        FLname = input("Enter a Name to Search By:\t")
-        db.selectRecord(FLname)
-
-    elif i == "3":
-        db.sortASC()
-
-    elif i == "4":
-        db.sortDESC()
-
-
-def output(table, record):
-    if table == "artist":
-        for row in record:
-            print("Full Name: ", row[0])
-            print("Phone #: ", row[1])
-            print("addy: ", row[2])
-            print("brith place: ", row[3])
-            print("Age: ", row[4])
-            print("Style of Art: ", row[5], '\n\n')
-    if table == "artshows":
-        for row in record:
-            print("Contact Number: ", row[0])
-            print("Contact: ", row[1])
-            print("Location: ", row[2])
-            print("Date of Show: ", row[3])
-            print("Time of Show: ", row[4], '\n\n')
-
-    if table == "artwork":
-        for row in record:
-            print("Type of Art: ", row[0])
-            print("Date Created: ", row[1])
-            print("Date Acquired: ", row[2])
-            print("Price of Art: ", row[3])
-            print("Location of Art: ", row[4])
-            print("Title of Art", row[5], '\n\n')
-
-    if table == "customer":
-        for row in record:
-            print("Customer's Art Preference: ", row[0])
-            print("Customer's Phone #: ", row[1])
-            print("Customer's Number in Line: ", row[2], '\n\n')
-
-
-print("\n\t"
-      "---Welcome to Art Gallery database simulation---\n")
-
-choice = input("Please select one of the following commands:"
-               "\n(1)Display Records from a Table\n(2)Select Specific Record From Artist table"
-               "\n(3)Order Customer Table by Customer Number (ASC)\n(4)Order Customer Table by "
-               "Customer Number (DESC)\n(q) Quit\n")
-
-while choice != "q" and choice != "Q":
-    action(choice)
-    choice = input("Please select one of the following commands:"
-                   "\n(1)Display Records from a Table\n(2)Select Specific Record From Artist table"
-                   "\n(3)Order Customer Table by Customer Number (ASC)\n(4)Order Customer Table by "
-                   "Customer Number (DESC)\n(q) Quit\n")
-else:
-    print("\n\tThank you and Bye!\n")
