d82059efa8f70d9ffd32f21bf2d71d64105d91c0
ChristopherMillones <ChristopherMillones@hotmail.com>
2020-06-05 00:29:22 -0700

    Completed

--- Assignment5.py
+++ Assignment5.py
@@ -4,74 +4,6 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from art_gallery import Ui_MainWindow
 import mysql
 from mysql.connector import Error
-import math
-
-class Art_Gallery:
-
-    def __init__(self, user, password, database):
-        try:
-            self.cxn = mysql.connector.connect(user=user, password=password, database=database)
-            # CONNECTING TO THE DATABASE
-            if self.cxn.is_connected():
-                db_info = self.cxn.get_server_info()
-                self.cursor = self.cxn.cursor(prepared=True)
-                self.cursor.execute("select database();")
-                record = self.cursor.fetchone()
-
-        except Error as e:
-            print("Error while connecting to MYSQL", e)
-
-    def __del__(self):
-        if self.cxn.is_connected():
-            self.cursor.close()
-            self.cxn.close()
-
-    def displayTable(self, table):
-        # SELECTING a TABLE and Displaying Information
-        self.cursor.execute("""SELECT * FROM {tab}""".format(tab=table))
-        record = self.cursor.fetchall()
-        return record
-
-    def selectRecordFromArtist(self, Data):
-        self.cursor.execute("""SELECT * FROM artist WHERE FLname= '{data}' OR phone= '{data}' OR address= '{data}' 
-           OR birthPlace= '{data}' OR age= '{data'}' OR styleOfArt= '{data}'""".format(data=Data))
-        record = self.cursor.fetchall()
-        return record
-
-    def selectRecordFromArtShows(self, Data):
-        self.cursor.execute("""SELECT * FROM artshows WHERE contactNumber= '{data}' OR contact= '{data}' OR location= '{data}' 
-            OR dateShow= '{data}' OR timeShow= '{data'}'""".format(data=Data))
-        record = self.cursor.fetchall()
-        return record
-
-    def selectRecordFromArtWork(self, Data):
-        self.cursor.execute("""SELECT * FROM artwork WHERE typeOfArt= '{data}' OR dateCreated= '{data}' OR dateAcquired= '{data}' 
-            OR price= '{data}' OR location= '{data'}' OR title='{data{'""".format(data=Data))
-        record = self.cursor.fetchall()
-        return record
-
-    def selectRecordFromCustomer(self, Data):
-        self.cursor.execute("""SELECT * FROM Customer WHERE artPreference= '{data}' OR phone= '{data}' 
-           OR customerNumber= '{data}' """.format(data=Data))
-        record = self.cursor.fetchall()
-        return record
-
-    def sortASC(self):
-        self.cursor.execute("""SELECT * FROM customer ORDER BY customerNumber ASC """)
-        record = self.cursor.fetchall()
-        return record
-
-    def sortDESC(self):
-        self.cursor.execute("""SELECT * FROM customer ORDER BY customerNumber DESC """)
-        record = self.cursor.fetchall()
-        return record
-
-    def getColumnHeaders(self, table):
-        self.cursor.execute("""SHOW COLUMNS FROM {tab}""".format(tab=table))
-        record = self.cursor.fetchall()
-        return record
-
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -81,6 +13,8 @@ class MainWindow(QMainWindow):
         self.PrintRecords()
         self.ok_btn_1()
         self.home_btn()
+        self.ok_btn_2()
+        self.SelectRecords()
         self.setWindowTitle("Art Gallery DB Simulation")
 
         try:
@@ -96,8 +30,6 @@ class MainWindow(QMainWindow):
         except Error as e:
             print("Error while connecting to MYSQL", e)
 
-
-
     def getColumnHeaders(self, table):
         self.cursor.execute("""SHOW COLUMNS FROM {tab}""".format(tab=table))
         record = self.cursor.fetchall()
@@ -112,6 +44,12 @@ class MainWindow(QMainWindow):
     def PrintRecords(self):
         self.ui.printRecords.clicked.connect(self.menu1)
 
+    def SelectRecords(self):
+        self.ui.selectRecords.clicked.connect(self.menu2)
+
+    def menu2(self):
+        self.ui.stackedWidget.setCurrentIndex(2)
+
     def menu1(self):
         self.ui.stackedWidget.setCurrentIndex(1)
 
@@ -123,6 +61,68 @@ class MainWindow(QMainWindow):
 
     def home_btn(self):
         self.ui.home_btn.clicked.connect(self.home)
+
+    def ok_btn_2(self):
+        self.ui.ok_bttn_2.clicked.connect(self.menu_2)
+
+
+    def menu_2(self):
+        print(self.ui.lineEdit.text() + "    Table: " + self.ui.table_select_2.currentText())
+        rows = self.getColumnHeaders(self.ui.table_select_2.currentText().replace(" ", "").lower())
+        self.col = []
+
+        for row in rows:
+            self.col.append(row[0])
+
+        self.ui.table_widget_2.setColumnCount(len(self.col))
+        self.ui.table_widget_2.setHorizontalHeaderLabels(self.col)
+
+
+        table = self.ui.table_select_2.currentText().replace(" ", "").lower()
+
+        if table == "artist":
+            data = self.selectRecordFromArtist(self.ui.lineEdit.text())
+            self.ui.table_widget_2.setRowCount(len(data))
+            self.insertDataIntoTable2(table,data)
+
+        if table == "artshows":
+            data = self.selectRecordFromArtShows(self.ui.lineEdit.text())
+            self.ui.table_widget_2.setRowCount(len(data))
+            self.insertDataIntoTable2(table,data)
+
+        if table == 'artwork':
+            data = self.selectRecordFromArtWork(self.ui.lineEdit.text())
+            self.ui.table_widget_2.setRowCount(len(data))
+            self.insertDataIntoTable2(table, data)
+
+        if table == 'customer':
+            data = self.selectRecordFromCustomer(self.ui.lineEdit.text())
+            self.ui.table_widget_2.setRowCount(len(data))
+            self.insertDataIntoTable2(table, data)
+
+    def selectRecordFromArtist(self, Data):
+        self.cursor.execute("""SELECT * FROM artist WHERE FLname= '{data}' OR phone= '{data}' OR address= '{data}' 
+        OR birthPlace= '{data}' OR age= '{data}' OR styleOfArt= '{data}'""".format(data=Data))
+        record = self.cursor.fetchall()
+        return record
+
+    def selectRecordFromArtShows(self, Data):
+        self.cursor.execute("""SELECT * FROM artshows WHERE contactNumber= '{data}' OR contact= '{data}' OR location= '{data}' 
+            OR dateShow= '{data}' OR timeShow= '{data}'""".format(data=Data))
+        record = self.cursor.fetchall()
+        return record
+
+    def selectRecordFromArtWork(self, Data):
+        self.cursor.execute("""SELECT * FROM artwork WHERE typeOfArt= '{data}' OR dateCreated= '{data}' OR dateAcquired= '{data}' 
+            OR price= '{data}' OR location= '{data}' OR title='{data}'""".format(data=Data))
+        record = self.cursor.fetchall()
+        return record
+
+    def selectRecordFromCustomer(self, Data):
+        self.cursor.execute("""SELECT * FROM Customer WHERE artPreference= '{data}' OR phone= '{data}' 
+           OR customerNumber= '{data}' """.format(data=Data))
+        record = self.cursor.fetchall()
+        return record
 
 
     def test(self):
@@ -143,17 +143,78 @@ class MainWindow(QMainWindow):
     def insertDataIntoTable(self,table,data):
 
         if table == "artist":
-                for x,row in zip(range(len(data)), data):
-                    self.ui.table_widget_1.setItem(x, 0, QTableWidgetItem(row[0]))
-                    self.ui.table_widget_1.setItem(x, 1, QTableWidgetItem(str(row[1])))
-                    self.ui.table_widget_1.setItem(x, 2, QTableWidgetItem(row[2]))
-                    self.ui.table_widget_1.setItem(x, 3, QTableWidgetItem(row[3]))
-                    self.ui.table_widget_1.setItem(x, 4, QTableWidgetItem(str(row[4])))
-                    self.ui.table_widget_1.setItem(x, 5, QTableWidgetItem(row[5]))
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_1.setItem(x, 0, QTableWidgetItem(row[0]))
+                self.ui.table_widget_1.setItem(x, 1, QTableWidgetItem(str(row[1])))
+                self.ui.table_widget_1.setItem(x, 2, QTableWidgetItem(row[2]))
+                self.ui.table_widget_1.setItem(x, 3, QTableWidgetItem(row[3]))
+                self.ui.table_widget_1.setItem(x, 4, QTableWidgetItem(str(row[4])))
+                self.ui.table_widget_1.setItem(x, 5, QTableWidgetItem(row[5]))
+            self.ui.table_widget_1.setSortingEnabled(True)
 
+        if table == "artshows":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_1.setItem(x, 0, QTableWidgetItem(str(row[0])))
+                self.ui.table_widget_1.setItem(x, 1, QTableWidgetItem(row[1]))
+                self.ui.table_widget_1.setItem(x, 2, QTableWidgetItem(row[2]))
+                self.ui.table_widget_1.setItem(x, 3, QTableWidgetItem(str(row[3])))
+                self.ui.table_widget_1.setItem(x, 4, QTableWidgetItem(row[4]))
+            self.ui.table_widget_1.setSortingEnabled(True)
 
+        if table == "artwork":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_1.setItem(x, 0, QTableWidgetItem(row[0]))
+                self.ui.table_widget_1.setItem(x, 1, QTableWidgetItem(str(row[1])))
+                self.ui.table_widget_1.setItem(x, 2, QTableWidgetItem(str(row[2])))
+                self.ui.table_widget_1.setItem(x, 3, QTableWidgetItem(str(row[3])))
+                self.ui.table_widget_1.setItem(x, 4, QTableWidgetItem(row[4]))
+                self.ui.table_widget_1.setItem(x, 5, QTableWidgetItem(row[5]))
+            self.ui.table_widget_1.setSortingEnabled(True)
 
+        if table == "customer":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_1.setItem(x, 0, QTableWidgetItem(row[0]))
+                self.ui.table_widget_1.setItem(x, 1, QTableWidgetItem(str(row[1])))
+                self.ui.table_widget_1.setItem(x, 2, QTableWidgetItem(str(row[2])))
+            self.ui.table_widget_1.setSortingEnabled(True)
 
+    def insertDataIntoTable2(self,table,data):
+
+        if table == "artist":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_2.setItem(x, 0, QTableWidgetItem(row[0]))
+                self.ui.table_widget_2.setItem(x, 1, QTableWidgetItem(str(row[1])))
+                self.ui.table_widget_2.setItem(x, 2, QTableWidgetItem(row[2]))
+                self.ui.table_widget_2.setItem(x, 3, QTableWidgetItem(row[3]))
+                self.ui.table_widget_2.setItem(x, 4, QTableWidgetItem(str(row[4])))
+                self.ui.table_widget_2.setItem(x, 5, QTableWidgetItem(row[5]))
+            self.ui.table_widget_2.setSortingEnabled(True)
+
+        if table == "artshows":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_2.setItem(x, 0, QTableWidgetItem(str(row[0])))
+                self.ui.table_widget_2.setItem(x, 1, QTableWidgetItem(row[1]))
+                self.ui.table_widget_2.setItem(x, 2, QTableWidgetItem(row[2]))
+                self.ui.table_widget_2.setItem(x, 3, QTableWidgetItem(str(row[3])))
+                self.ui.table_widget_2.setItem(x, 4, QTableWidgetItem(row[4]))
+            self.ui.table_widget_2.setSortingEnabled(True)
+
+        if table == "artwork":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_2.setItem(x, 0, QTableWidgetItem(row[0]))
+                self.ui.table_widget_2.setItem(x, 1, QTableWidgetItem(str(row[1])))
+                self.ui.table_widget_2.setItem(x, 2, QTableWidgetItem(str(row[2])))
+                self.ui.table_widget_2.setItem(x, 3, QTableWidgetItem(str(row[3])))
+                self.ui.table_widget_2.setItem(x, 4, QTableWidgetItem(row[4]))
+                self.ui.table_widget_2.setItem(x, 5, QTableWidgetItem(row[5]))
+            self.ui.table_widget_2.setSortingEnabled(True)
+
+        if table == "customer":
+            for x,row in zip(range(len(data)), data):
+                self.ui.table_widget_2.setItem(x, 0, QTableWidgetItem(row[0]))
+                self.ui.table_widget_2.setItem(x, 1, QTableWidgetItem(str(row[1])))
+                self.ui.table_widget_2.setItem(x, 2, QTableWidgetItem(str(row[2])))
+            self.ui.table_widget_2.setSortingEnabled(True)
 
 
 if __name__ == "__main__":
--- art_gallery.py
+++ art_gallery.py
@@ -26,17 +26,22 @@ class Ui_MainWindow(object):
         self.stackedWidget = QStackedWidget(self.centralwidget)
         self.stackedWidget.setObjectName(u"stackedWidget")
         self.stackedWidget.setGeometry(QRect(10, 40, 981, 541))
+        self.stackedWidget.setStyleSheet(u"background-color: rgb(163, 163, 163);")
         self.main_menu = QWidget()
         self.main_menu.setObjectName(u"main_menu")
         self.printRecords = QPushButton(self.main_menu)
         self.printRecords.setObjectName(u"printRecords")
-        self.printRecords.setGeometry(QRect(280, 90, 201, 41))
+        self.printRecords.setGeometry(QRect(370, 80, 201, 41))
+        self.printRecords.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.textBrowser = QTextBrowser(self.main_menu)
         self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setGeometry(QRect(230, 30, 291, 41))
-        self.pushButton = QPushButton(self.main_menu)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(280, 170, 201, 51))
+        self.textBrowser.setGeometry(QRect(330, 10, 291, 41))
+        self.textBrowser.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.selectRecords = QPushButton(self.main_menu)
+        self.selectRecords.setObjectName(u"selectRecords")
+        self.selectRecords.setGeometry(QRect(370, 160, 201, 51))
+        self.selectRecords.setStyleSheet(u"\n"
+"background-color: rgb(255, 255, 255);")
         self.stackedWidget.addWidget(self.main_menu)
         self.print_records = QWidget()
         self.print_records.setObjectName(u"print_records")
@@ -47,24 +52,30 @@ class Ui_MainWindow(object):
         self.table_select_1.addItem("")
         self.table_select_1.setObjectName(u"table_select_1")
         self.table_select_1.setGeometry(QRect(530, 70, 101, 21))
+        self.table_select_1.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.label = QLabel(self.print_records)
         self.label.setObjectName(u"label")
         self.label.setGeometry(QRect(430, 70, 81, 21))
+        self.label.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.textBrowser_3 = QTextBrowser(self.print_records)
         self.textBrowser_3.setObjectName(u"textBrowser_3")
         self.textBrowser_3.setGeometry(QRect(0, 70, 351, 131))
+        self.textBrowser_3.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.ok_bttn_1 = QPushButton(self.print_records)
         self.ok_bttn_1.setObjectName(u"ok_bttn_1")
         self.ok_bttn_1.setGeometry(QRect(650, 70, 81, 21))
+        self.ok_bttn_1.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.table_widget_1 = QTableWidget(self.print_records)
         self.table_widget_1.setObjectName(u"table_widget_1")
         self.table_widget_1.setGeometry(QRect(360, 100, 621, 391))
+        self.table_widget_1.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.stackedWidget.addWidget(self.print_records)
         self.select_record = QWidget()
         self.select_record.setObjectName(u"select_record")
         self.textBrowser_4 = QTextBrowser(self.select_record)
         self.textBrowser_4.setObjectName(u"textBrowser_4")
         self.textBrowser_4.setGeometry(QRect(10, 10, 361, 121))
+        self.textBrowser_4.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.table_select_2 = QComboBox(self.select_record)
         self.table_select_2.addItem("")
         self.table_select_2.addItem("")
@@ -72,18 +83,24 @@ class Ui_MainWindow(object):
         self.table_select_2.addItem("")
         self.table_select_2.setObjectName(u"table_select_2")
         self.table_select_2.setGeometry(QRect(490, 20, 101, 21))
+        self.table_select_2.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.label_2 = QLabel(self.select_record)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setGeometry(QRect(390, 20, 81, 21))
+        self.label_2.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.ok_bttn_2 = QPushButton(self.select_record)
         self.ok_bttn_2.setObjectName(u"ok_bttn_2")
         self.ok_bttn_2.setGeometry(QRect(620, 110, 81, 21))
+        self.ok_bttn_2.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.table_widget_2 = QTableWidget(self.select_record)
         self.table_widget_2.setObjectName(u"table_widget_2")
         self.table_widget_2.setGeometry(QRect(40, 180, 691, 271))
+        self.table_widget_2.setStyleSheet(u"background-color: rgb(255, 255, 255);")
         self.lineEdit = QLineEdit(self.select_record)
         self.lineEdit.setObjectName(u"lineEdit")
         self.lineEdit.setGeometry(QRect(400, 110, 191, 21))
+        self.lineEdit.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.lineEdit.setMaxLength(20)
         self.stackedWidget.addWidget(self.select_record)
         self.textBrowser_2 = QTextBrowser(self.centralwidget)
         self.textBrowser_2.setObjectName(u"textBrowser_2")
@@ -91,6 +108,7 @@ class Ui_MainWindow(object):
         self.home_btn = QPushButton(self.centralwidget)
         self.home_btn.setObjectName(u"home_btn")
         self.home_btn.setGeometry(QRect(30, 0, 71, 31))
+        self.home_btn.setStyleSheet(u"background-color: rgb(85, 255, 255);")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -102,7 +120,7 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
 
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(2)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -116,7 +134,7 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#00007f;\">Welcome to the Main Menu!</span></p></body></html>", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Select Record", None))
+        self.selectRecords.setText(QCoreApplication.translate("MainWindow", u"Select Record", None))
         self.table_select_1.setItemText(0, QCoreApplication.translate("MainWindow", u"Artist", None))
         self.table_select_1.setItemText(1, QCoreApplication.translate("MainWindow", u"Art Shows", None))
         self.table_select_1.setItemText(2, QCoreApplication.translate("MainWindow", u"Art Work", None))
@@ -137,9 +155,10 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#00007f;\">How to use:</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#00007f;\">Use the Combo Box to select and table then click the &quot;Ok&quot; Button to confirm and display information.</span></p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt; color:#00007f;\"><br /></p></b"
-                        "ody></html>", None))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; color:#00007f;\">Select a table then input data to see if it exist within the database or table</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt; color:#00007f;\"><br /></p>\n"
+"<p style=\"-qt-paragraph-type:"
+                        "empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:14pt; color:#00007f;\"><br /></p></body></html>", None))
         self.table_select_2.setItemText(0, QCoreApplication.translate("MainWindow", u"Artist", None))
         self.table_select_2.setItemText(1, QCoreApplication.translate("MainWindow", u"Art Shows", None))
         self.table_select_2.setItemText(2, QCoreApplication.translate("MainWindow", u"Art Work", None))
@@ -147,6 +166,7 @@ class Ui_MainWindow(object):
 
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Select a Table:", None))
         self.ok_bttn_2.setText(QCoreApplication.translate("MainWindow", u"Search", None))
+        self.lineEdit.setText(QCoreApplication.translate("MainWindow", u"type here....", None))
         self.textBrowser_2.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
